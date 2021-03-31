35458d892bc69ecb2f525aaa0c86b11504ed43f6
Jonathan Axford <jon.axford@outlook.com>
2021-03-30 18:03:46 -0600

    Initial configuration and the addition of phpMyAdmin
    Co-authored-by: Latonia To <lto417@users.noreply.github.com>

---
+++ src/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'companies');
+define('DBUSER', 'MARIA-USER');
+define('DBPASS', 'MARIA-USERR-PW');
+
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
