b9586c2394f4303d6d0cb36020618bb1566f8893
Latonia <lto417@mtroyal.ca>
2021-04-02 01:33:22 -0600

    updated basic layout

--- src/index.php
+++ src/index.php
@@ -12,7 +12,8 @@ echo '<body><main class="container">';
 
 generateHead();
 echo '<h1 class="t" id="title">Stock Browser</h1>';
-generateButtons();
+//generateButtons();
+generateAllButtons();
 echo '</body></main>';
 
 function generateHead(){?>
@@ -30,26 +31,58 @@ function generateHead(){?>
 function generateButtons(){?>
     <div class="box a">
         <form action="about.php" method="get">
-            <button class="buttonIcons" type="submit" value="about"><i class="far fa-question-circle"></i></i>About</button>
+            <button class="buttonIcons" type="submit" value="about"><i class="far fa-question-circle"></i> About</button>
         </form>
     </div>
     <div class="box b">
         <form action="list.php" method="get">
-            <button class="buttonIcons" type="submit" value="list"><i class="fas fa-building"></i>Companies</button>
+            <button class="buttonIcons" type="submit" value="list"><i class="fas fa-building"></i> Companies</button>
         </form>
     </div>
     <div class="box c">
         <form action="login.php" method="get">
-            <button class="buttonIcons" type="submit" value="list"><i class="fas fa-sign-in-alt"></i></i>Login</button>
+            <button class="buttonIcons" type="submit" value="login"><i class="fas fa-sign-in-alt"></i> Login</button>
         </form>
     </div>
     <div class="box d">
         <form action="coming-soon.php" method="get">
-            <button class="buttonIcons" type="submit" value="list"><i class="fas fa-user-plus"></i></i></i>Sign Up</button>
+            <button class="buttonIcons" type="submit" value="signup"><i class="fas fa-user-plus"></i> Sign Up</button>
         </form>
     </div>
-
 <?php
 }
+function generateAllButtons(){?>
+    <div class="box a">
+        <form action="about.php" method="get">
+            <button class="buttonIcons" type="submit" value="about"><i class="far fa-question-circle"></i> About</button>
+        </form>
+    </div>
+    <div class="box b">
+        <form action="list.php" method="get">
+            <button class="buttonIcons" type="submit" value="list"><i class="fas fa-building"></i> Companies</button>
+        </form>
+    </div>
+    <div class="box c">
+        <form action="coming-soon.php" method="get">
+            <button class="buttonIcons" type="submit" value="portfolio"><i class="fas fa-clipboard-list"></i> Portfolio</button>
+        </form>
+    </div>
+    <div class="box d">
+        <form action="favorites.php" method="get">
+            <button class="buttonIcons" type="submit" value="favorite"><i class="fas fa-star"></i> Favorites</button>
+        </form>
+    </div>
+    <div class="box e">
+        <form action="favorites.php" method="get">
+            <button class="buttonIcons" type="submit" value="profile"><i class="fas fa-address-card"></i> Profile</button>
+        </form>
+    </div>
+    <div class="box f">
+        <form action="favorites.php" method="get">
+            <button class="buttonIcons" type="submit" value="logout"><i class="fas fa-sign-out-alt"></i> Logout</button>
+        </form>
+    </div>
+<?php
 
+}
 ?>
