980c4b6454471a5963759aa0afd4f94f556c4786
Jonathan Axford <jon.axford@outlook.com>
2021-04-12 20:02:41 -0600

    Basic favorites

--- src/favorites.php
+++ src/favorites.php
@@ -1,58 +1,9 @@
 <?php
-require_once 'config.inc.php';
-require_once 'index-db-classes.inc.php';
-require_once 'index-db-classes.inc.php';
 require_once 'header.inc.php';
-try {
-    $conn = DatabaseHelper::createConnection(array(
-        DBCONNSTRING,
-        DBUSER, DBPASS
-    ));
-    if (isset($_GET['history'])) {
-        $symbol = $_GET["history"];
-        SessionManager::upsertSessionVar('symbol', $symbol);
-        $gateway = new HistoryDB($conn);
-        $history = $gateway->getAllForHistory($symbol);
-    } else if (isset($_GET['sort'])) {
-        $gateway = new HistoryDB($conn);
-        $symbol = SessionManager::getSessionVar('symbol');
-        $history = $gateway->getSortedAllForHistory($symbol, $_GET["sort"]);
-    } else {
-        echo "Error";
-    }
-} catch (Exception $e) {
-    die($e->getMessage());
-}
+require_once 'index-db-classes.inc.php';
 
-function displayHistory($history)
-{
-    echo "<h1>Company Stock Data</h1>";
-    echo "<img id='historyLogo' src='/logos/" . SessionManager::getSessionVar('symbol') . ".svg'>";
-    echo "<form action='history.php' method='get'>";
-    echo "<div id='table'>";
-    echo "<table id='stock'>";
-    echo "<tr id='stockHeaders'>";
-    echo "<th><a href=''>Date</a></th>";
-    echo "<th><a href='?sort=volume'>Volume</a></th>";;
-    echo "<th>Open</th>";
-    echo "<th>Close</th>";
-    echo "<th>High</th>";
-    echo "<th>Low</th>";
-    echo "</tr>";
-    foreach ($history as $key => $value) { ?>
-        <tr>
-            <td><?= $value['date']; ?></td>
-            <td><?= $value['volume']; ?></td>
-            <td><?= $value['open']; ?></td>
-            <td><?= $value['close']; ?></td>
-            <td><?= $value['high']; ?></td>
-            <td><?= $value['low']; ?></td>
-        </tr>
-<?php
-    }
-    echo "</table>";
-    echo "</div>";
-}
+$favs = SessionManager::getFavorites();
+
 ?>
 
 
@@ -61,14 +12,40 @@ function displayHistory($history)
 
 <head>
     <meta charset="utf-8" />
-    <title>Your Favorites</title>
-
+    <title>Company Financials</title>
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php displayHistory($history) ?>
+
+    <div>
+        <?php drawHeader("Favorite Companies"); ?>
+        <form class=textbox action="" method="POST">
+            <fieldset>
+                <ul id="companyList">
+                    <?php
+                    if(isset($favs)){
+                        foreach ($favs as $symbol=>$details) {
+                            echo "<li> ";
+                            echo '<img class="smallLogo" src="/logos/' .$details['symbol'] . '.svg" alt="' . $details['symbol'] . '">';
+                            echo $details['symbol'] . " " . $details['name']; 
+                            echo '<button class="infoButtons" type="submit" value="' . $details['symbol'] . '">';
+                            echo '<span class="fa-stack fa-2x">';
+                            echo '<i class="fas fa-star fa-stack-2x fa-xs"></i>';
+                            echo '<i id="dark" class="fas fa-minus fa-stack-1x fa-xs"></i>';
+                            echo '</span>';
+                            echo "Remove</button>";
+                            echo"</li>";
+                        }
+                    } else {
+                        echo "<li>No Favorites saved!</i>";
+                    }
+                    ?>
+                </ul>
+            </fieldset>
+        </form>
+    </div>
 </body>
 
 </html>
