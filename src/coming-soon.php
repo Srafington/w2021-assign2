96aef1d4354a388f2417c3a889309fe20d876379
Latonia <lto417@mtroyal.ca>
2021-04-12 21:17:21 -0600

    coming-soon page

---
+++ src/coming-soon.php
@@ -0,0 +1,30 @@
+
+<?php
+
+function generateDisplay(){?>
+    <main class="box">
+        <div id="csBox">
+            <div id="csIcon"><i class="fas fa-pencil-ruler"></i><i class="fas fa-upload"></i><i class="fas fa-laptop-code"></i></div>
+            <div><h2>COMING SOON</h2></div>
+        </div>
+    </main>
+<?php
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Company Financials</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php generateDisplay() ?>
+</body>
+</html>
