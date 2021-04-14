40b437b562c48c333b48433c15f034e3dd9ea9b3
Jonathan Axford <jon.axford@outlook.com>
2021-04-13 19:27:49 -0600

    php database config for live environment

--- src/config.inc.php
+++ src/config.inc.php
@@ -1,7 +1,7 @@
 <?php
-define('DBHOST', 'db');
-define('DBNAME', 'stocks');
-define('DBUSER', 'MARIA-USER');
-define('DBPASS', 'MARIA-USERR-PW');
+define('DBHOST', 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DBNAME', 'x3rtvbbl5ms9ojyz');
+define('DBUSER', 'c3ybewfii4esptbw');
+define('DBPASS', 'zsbjvamqqrj0k5ns');
 
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
