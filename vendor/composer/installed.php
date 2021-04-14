cc7e2f5b96aee39b23fcda8fbef42cbd385e32be
Jonathan Axford <jon.axford@outlook.com>
2021-04-13 20:14:52 -0600

    Heroku deploy config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '85b7b78b9c340617a66e95caf554286b50372fb3',
+    'name' => 'sidew/w2021-assign2',
+  ),
+  'versions' => 
+  array (
+    'sidew/w2021-assign2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '85b7b78b9c340617a66e95caf554286b50372fb3',
+    ),
+  ),
+);
