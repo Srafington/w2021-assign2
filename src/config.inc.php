119f11ed1f8ffdf2009bc0e8b9f568e81e385f7f
Jonathan Axford <44613194+Srafington@users.noreply.github.com>
2021-04-25 05:42:33 -0600

    Delete config.inc.php

--- src/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('DBNAME', 'x3rtvbbl5ms9ojyz');
-define('DBUSER', 'c3ybewfii4esptbw');
-define('DBPASS', 'zsbjvamqqrj0k5ns');
-
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
